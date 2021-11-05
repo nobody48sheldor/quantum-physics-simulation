bacb241b2fb5e36725fd6bad846b9f35be0a3006
nobody48sheldor <lomyizi@gmail.com>
2021-11-05 02:03:24 +0100

    finally working

--- quantum-physics.py
+++ quantum-physics.py
@@ -3,17 +3,23 @@ import numpy as np
 import cmath
 from mpl_toolkits.mplot3d import axes3d
 from matplotlib import cm
+import cython
 import os
 
-L = 10
-n = 100
-nt = 400*n
-alpha = 4
+L = 20
+n = 300
+nt = int(200*n/6)
+alpha = 2
 m = 1
+h = 6.62607015 * (10**(-34))
+hbar = 1
 
-x =np.linspace(0, L, n)
-y =np.linspace(0, L, n)
-t =np.linspace(0, 100, nt)
+T = 2*L/(400)
+print(T)
+
+x =np.linspace(-L, L, n)
+y =np.linspace(-L, L, n)
+t =np.linspace(0, 60, nt)
 
 dx = x[1] - x[0]
 dy = y[1] - y[0]
@@ -33,7 +39,7 @@ def psi_0(x, y):
         Sxr = []
         Sxi = []
         for i in x:
-            s = (1+1j) * np.exp(-((i-L/2)**2 + (w-L/2)**2)/alpha) / (2*np.pi*alpha)
+            s = (1-1j) * ((np.exp(-((i-L/2)**2 + (w-L/2)**2)/alpha)) / (2*np.pi*alpha))
             Sx.append(s)
             Sxr.append(s.real)
             Sxi.append(s.imag)
@@ -53,15 +59,16 @@ ax = fig.add_subplot(211, projection = '3d')
 ax.plot_surface(X, Y, np.array(psi0r), cmap = cm.plasma, linewidth=0, antialiased=True)
 ax.axes.set_xlim3d(left=0, right=L)
 ax.axes.set_ylim3d(bottom=0, top=L)
-ax.axes.set_zlim3d(bottom=-0.1, top=0.1)
+ax.axes.set_zlim3d(bottom=-1, top=1)
 
 def V(x, y):
     S = []
     for w in y:
         Sx = []
         for i in x:
-            Sx.append(10*np.exp(-(i**2 + w**2)/10))
-        S.append(Sx)
+            s = 10*np.exp(0.1*(w+i-L))
+            Sx.append(s)
+        S.append(np.array(Sx))
     return(S)
 
 V = V(x, y)
@@ -91,6 +98,23 @@ psiyI = []
 
 psi = 0
 
+def plot():
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection = '3d')
+    plt.ion()
+
+    for i in range(nt):
+        plt.clf()
+        ax = fig.add_subplot(111, projection = '3d')
+        plt.title('Time = {} secondes'.format(t[i]))
+        ax.plot_surface(X, Y, PSIR[i], cmap = cm.plasma, linewidth=0, antialiased=True)
+        ax.axes.set_xlim3d(left=-L, right=L)
+        ax.axes.set_ylim3d(bottom=-L, top=L)
+        ax.axes.set_zlim3d(bottom=-0.2, top=0.4)
+        if i%5 == 0:
+            plt.savefig("{}.png".format(i))
+        plt.pause(dt)
+
 for i in range(1, nt - 2):
     os.system("clear")
     print(int(100*i/nt), " %")
@@ -109,10 +133,14 @@ for i in range(1, nt - 2):
         psixR.append(PSIR[i][0][0])
         psixI.append(PSII[i][0][0])
         for u in range(1, n-1):
-            psi = PSI[i][v][u] + ((1j/(2*m)) * ((PSI[i][v][u+1] - 2*PSI[i][v][u] + PSI[i][v][u-1] + PSI[i-1][v][u+1] - 2*PSI[i-1][v][u] + PSI[i-1][v][u-1])/(2*dx*dx) + (PSI[i][v+1][u] - 2*PSI[i][v][u] + PSI[i][v-1][u] + PSI[i-1][v+1][u] - 2*PSI[i-1][v][u] + PSI[i-1][v-1][u])/(2*dy*dy)) + V[v][u]*PSI[i][v][u])*dt
-            psix.append(psi)
-            psixR.append(psi.real)
-            psixI.append(psi.imag)
+            try:
+                psi = PSI[i-1][v][u] + ((1j/2*hbar) * ( ((hbar*hbar/(2*m)) * (0.5*(PSI[i][v][u+1] - 2*PSI[i][v][u] + PSI[i][v][u-1] + PSI[i-1][v][u+1] - 2*PSI[i-1][v][u] + PSI[i-1][v][u-1])/(2*dx*dx) + 0.5*(PSI[i][v+1][u] - 2*PSI[i][v][u] + PSI[i][v-1][u] + PSI[i-1][v+1][u] - 2*PSI[i-1][v][u] + PSI[i-1][v-1][u])/(2*dy*dy)) + V[v][u]*PSI[i][v][u]))*dt) 
+                psix.append(psi)
+                psixR.append(psi.real)
+                psixI.append(psi.imag)
+            except KeyboardInterrupt:
+                plot()
+
 
         psix.append(psi)
         psixR.append(psi.real)
@@ -130,18 +158,5 @@ for i in range(1, nt - 2):
     PSIR.append(np.array(psiyR))
     PSII.append(np.array(psiyI))
 
-fig = plt.figure()
-ax = fig.add_subplot(111, projection = '3d')
-plt.ion()
-
 input("")
-
-for i in range(nt):
-    plt.clf()
-    ax = fig.add_subplot(111, projection = '3d')
-    plt.title('Time = {} secondes'.format(t[i]))
-    ax.plot_surface(X, Y, PSIR[i], cmap = cm.plasma, linewidth=0, antialiased=True)
-    ax.axes.set_xlim3d(left=0, right=L)
-    ax.axes.set_ylim3d(bottom=0, top=L)
-    ax.axes.set_zlim3d(bottom=-0.1, top=0.1)
-    plt.pause(dt)
+plot()
